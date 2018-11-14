fe29234b911bc9af1314f9ad5c90ce0b68096d9e
kovitx <sllonk@gmail.com>
2018-11-14 09:22:09 +0100

    pictures

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,6 +8,7 @@ const app = express()
 const port = 3000
 
 // Routes
+app.use(express.static("public"));
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/about', (req, res) => res.sendFile(__dirname + '/views/about.html'));
 app.get('/gallery', (req, res) => res.sendFile(__dirname + '/views/gallery.html'));
