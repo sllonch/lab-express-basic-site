783d1f579a906d363a2b91b67df0f1919ad94e32
kovitx <sllonk@gmail.com>
2018-11-14 09:31:46 +0100

    pictures

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,8 +7,10 @@ const app = express()
 // Defining Port
 const port = 3000
 
-// Routes
-app.use(express.static("public"));
+// Middleware
+app.use(express.static('public'));
+
+//Routes
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/about', (req, res) => res.sendFile(__dirname + '/views/about.html'));
 app.get('/gallery', (req, res) => res.sendFile(__dirname + '/views/gallery.html'));
